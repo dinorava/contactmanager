ef04883b4374b7237f26fb5850a225352dacd46d
Dino Rava <dinorava2@hotmail.com>
2019-04-04 01:02:08 +0200

    Updates

---
+++ precache-manifest.be0939a6512a4d0b159c2d2fc8012946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "72ff14cfd2ffa4620379",
+    "url": "/contactmanager/static/js/main.da246c23.chunk.js"
+  },
+  {
+    "revision": "90b4a101295c46822af5",
+    "url": "/contactmanager/static/js/2.c0499e9f.chunk.js"
+  },
+  {
+    "revision": "72ff14cfd2ffa4620379",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "90b4a101295c46822af5",
+    "url": "/contactmanager/static/css/2.558248d9.chunk.css"
+  },
+  {
+    "revision": "4332a6ecdc8fa48edabbfecd8755d4c3",
+    "url": "/contactmanager/index.html"
+  }
+];
